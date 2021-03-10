37c5debcc79eb6b98b7e6e39273e197268de0c3b
Giulio Pastorello <giulio.pastorello@gmail.com>
2021-03-10 10:24:15 +0100

    esercizio sulle potenze

---
+++ ex3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int power (int base, int exp){
+    int result = 1;
+    for (int i = 0; i < exp; i++) {
+        result = result * base;
+    }
+    return result;
+}
+
+int main()
+{
+    int base, exp;
+    cout << "Enter base: ";
+    cin >> base;
+    cout << "Enter exponent: ";
+    cin >> exp;
+
+    cout << power (base, exp);
+    
+    return 0;    
+}
